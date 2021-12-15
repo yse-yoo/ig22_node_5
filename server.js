ac8d6a0cf5c89c0bea556c6f77112d8d08274ff6
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-12-15 10:09:57 +0900

    modify server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const uuidv4 = require('uuid').v4
+
+const dotenv = require('dotenv');
+dotenv.config();
+const host = process.env.HOST
+const port = process.env.PORT
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+http.listen(port, host, () => {
+    console.log('http://' + host + ':' + port)
+})
